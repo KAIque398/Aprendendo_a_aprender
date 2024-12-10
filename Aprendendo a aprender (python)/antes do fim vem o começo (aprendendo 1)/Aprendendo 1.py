e72ccb26ae894bc549314e596c6e3430f91d74b4
KAIque398 <145579082+KAIque398@users.noreply.github.com>
2024-12-10 15:50:02 -0300

    Delete Aprendendo a aprender (python) directory

--- Aprendendo a aprender (python)/antes do fim vem o começo (aprendendo 1)/Aprendendo 1.py
+++
@@ -1,6 +0,0 @@
-nome = input ("qual seu nome?")
-idade = input ("qual a sua idade?")
-peso = input ("quanto você pesa?")
-print (nome, idade, peso)
-
-
--- Aprendendo a aprender (python)/antes do fim vem o começo (aprendendo 1)/exercicios/exercicios 001.py
+++
@@ -1,7 +0,0 @@
-nome = input ("qual o seu nome?")
-
-print ("olá, {}! Prazer em te conhecer" .format (nome)) 
-
-
-
-
