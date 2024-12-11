b5686a26eb2e14922f4dd7dbf8b8724287713b88
KAIque398 <145579082+KAIque398@users.noreply.github.com>
2024-12-10 21:53:33 -0300

    Add files via upload

---
+++ exercicios 004.py
@@ -0,0 +1,16 @@
+a = input ("Digite qualquer coisa: ")
+
+print ("O tipo do prompt é:", type(a)) 
+print ("O prompt é númerico?", a.isnumeric())
+print ("O prompt é alfabético?", a.isalpha())
+print ("O prompt é alfanumérico?", a.isalnum())
+print ("O prompt é somente espaços?", a.isspace())
+print ("O prompt está em maiusculo?", a.isupper())
+print ("O prompt está em minusculo?", a.islower())
+print ("O prompt está capitalizada?", a.istitle())
+
+
+
+
+
+
